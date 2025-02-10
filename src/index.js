7f78812af9f7451fa632b3a957dbb5d476a8e8ef
Abhay Sharma <abhysharma5113@gmail.com>
2025-02-10 16:10:02 +0530

    exploring : APIs

--- src/index.js
+++ src/index.js
@@ -7,14 +7,12 @@ const User = require("./models/user");
 
 const app = express();
 
-app.post("/user", async(req, res) => {
+app.use(express.json());
+
+app.post("/user", async (req, res) => {
+  console.log(req.body);
   try {
-    const user = new User({
-      firstName: "Krishna",
-      lastName: "Sharma",
-      email: "Krishna@gmail.com",
-      gender: "male",
-    });
+    const user = new User(req.body);
 
     await user.save();
 
@@ -24,12 +22,74 @@ app.post("/user", async(req, res) => {
   }
 });
 
+app.get("/user", async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+   
+    if (!user) return res.status(404).send("User not found");
+
+
+    res.send(user);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
+ 
+app.get("/feed", async (req, res) => {
+  try {
+    const user = await User.find({});
+
+    res.send(user);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
+app.delete("/user", async (req, res) => {
+  try {
+    const user = await User.deleteOne({ email: req.body.email });
+
+    if (user.deletedCount === 0) {
+      res.send("User not found");
+    } else {
+      res.send(user);
+    }
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
+
+app.patch("/user" , async(req, res) => {
+
+  const data = req.body
+  const email = req.body.email
+
+  try {
+  
+     const user = await User.findOneAndReplace({email} , data , { returnDocument: "after" } )
+
+
+    res.send(user);
+
+
+    
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+})
+
 app.use("/", (err, req, res, next) => {
   res.status(500).send({ message: err.message });
 });
 
-dbConnect().then(() => {
-  app.listen(3000, () => {
-    console.log("Server is running");
+dbConnect()
+  .then(() => {
+    app.listen(3000, () => {
+      console.log("Server is running");
+    });
+  })
+  .catch((err) => {
+    console.log(err.message);
   });
-});
