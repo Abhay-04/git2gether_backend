facde9a3d75f00c2123cf0ca71a54b2caee961ab
Abhay Sharma <abhysharma5113@gmail.com>
2025-02-28 17:59:30 +0530

    whitelist : frontend origin

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173" , "https://git2gether.vercel.app" ],
     credentials: true,
   })
 );
